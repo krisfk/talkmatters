71bf01d45713998b4b78209e4a11af4c5858dde3
kaiyu li <krisfk@gmail.com>
2021-04-21 00:20:45 +0800

    .

--- wp-content/themes/talkmatters/page-home.php
+++ wp-content/themes/talkmatters/page-home.php
@@ -19,14 +19,14 @@ get_header();
 ?>
 <div class="container">
 
-    <div class="mint-color fw-bold home-slogan text-center mt-4">
+    <div class="mint-color fw-bold home-slogan text-center mt-5">
 
         We know there is always distress in life and we understand it could be <br>
         hard to deal with, live with them and even trying to face them.
 
     </div>
 
-    <div class="position-relative home-points-div mt-4">
+    <div class="position-relative home-points-div mt-5">
         <img src="http://104.131.45.33/wp-content/uploads/2021/04/home-img-1.jpg" alt="">
 
         <div class="home-points">
