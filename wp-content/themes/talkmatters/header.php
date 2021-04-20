4f857dfbf2c225bb3c3fc2cd6cb1923cbf2f66bc
kaiyu li <krisfk@gmail.com>
2021-04-20 23:49:24 +0800

    .

--- wp-content/themes/talkmatters/header.php
+++ wp-content/themes/talkmatters/header.php
@@ -67,22 +67,28 @@
                                 <li><a href="#">Contact</a></li>
 
                             </ul>
+
+                            <div class="d-inline-block">
+
+                                <a href="#">
+                                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/fb-icon.png"
+                                        alt="">
+                                </a>
+                                <a href="#">
+
+                                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/ig-icon.png"
+                                        alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/in-icon.png"
+                                        alt="">
+                                </a>
+
+                            </div>
+
                         </div>
 
 
-                        <div>
 
-                            <a href="#">
-                                <img src="<?php echo get_template_directory_uri() ?>/assets/images/fb-icon.png" alt="">
-                            </a>
-                            <a href="#">
-
-                                <img src="<?php echo get_template_directory_uri() ?>/assets/images/ig-icon.png" alt="">
-                            </a>
-                            <a href="#">
-                                <img src="<?php echo get_template_directory_uri() ?>/assets/images/in-icon.png" alt="">
-                            </a>
-
-                        </div>
 
                     </div>
