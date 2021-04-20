7e179032a64c87ea271d4448ddc25500cccf175d
kaiyu li <krisfk@gmail.com>
2021-04-20 18:28:48 +0800

    .

--- wp-content/themes/talkmatters/header.php
+++ wp-content/themes/talkmatters/header.php
@@ -31,4 +31,4 @@
 
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
-                <main id="main" class="site-main" role="main">
+                <main id="main" class="" role="main">
