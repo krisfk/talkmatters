4e8f22bf94ea8486c37771edd1ecc2a9f1fc6706
kaiyu li <krisfk@gmail.com>
2021-04-21 03:12:46 +0800

    .

--- wp-content/themes/talkmatters/header.php
+++ wp-content/themes/talkmatters/header.php
@@ -81,9 +81,9 @@ $slug=str_replace('/','',$slug);
 
 global $post;
 $post_slug = $post->post_name;
-echo $post_slug.'!'.$slug;
+
 echo '<li><a class="';
-echo ($post_slug==$slug  ) ? 'active' :'';
+echo ($post_slug==$slug || $post_slug=='home' && $slug ==''  ) ? 'active' :'';
 echo'" href="'.$url.'">'.$title.'</a>';
 
 
