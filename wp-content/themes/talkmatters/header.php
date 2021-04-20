7e86e0ccee0ce1230f367d91d947fd5d8807b8f3
kaiyu li <krisfk@gmail.com>
2021-04-21 03:01:41 +0800

    .

--- wp-content/themes/talkmatters/header.php
+++ wp-content/themes/talkmatters/header.php
@@ -79,33 +79,8 @@ $slug=str_replace('/en/','',$temp_arr[1]);
 $slug=str_replace('/cn/','',$slug);
 $slug=str_replace('/','',$slug);
 
-
-if(count($menu_item['children']))
-{
-   
-    echo '<li><a class="level-1 parent" href="'.$url.'">'.$title.'</a>';
-
- 
-    echo '<ul class="menu-submenu">';
-    foreach ($menu_item['children'] as $sub_menu_item) 
-    {
-        $sub_url = $sub_menu_item['url'];
-        $sub_title = $sub_menu_item['title'];
-        $sub_temp_arr=explode(get_site_url(),$sub_url);
-        $sub_slug=str_replace('/en/','',$sub_temp_arr[1]);
-        $sub_slug=str_replace('/cn/','',$sub_slug);
-        $sub_slug=str_replace('/','',$sub_slug);
-        echo'<li><a class="'.$sub_slug.'" href="'.$sub_url.'">'.$sub_title.'</a></li>';
-    }
-    echo '</ul>';
-
-}
-else
-{
 echo '<li><a class="level-1 '.$slug.'" href="'.$url.'">'.$title.'</a>';
 
-}
-echo'</li>';
 
 
 }
