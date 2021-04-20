10e0c372a35623ebfa41b0a08473263435b097a9
kaiyu li <krisfk@gmail.com>
2021-04-21 02:18:19 +0800

    .

--- wp-content/themes/talkmatters/page-home.php
+++ wp-content/themes/talkmatters/page-home.php
@@ -44,7 +44,8 @@ get_header();
     </div>
 
 
-    <div class="mt-5">As an emotional guide, we facilitate our clients to identify your feelings and the meaning behind
+    <div class="mt-5 inner-container">As an emotional guide, we facilitate our clients to identify your feelings and the
+        meaning behind
         these emotions. We help to make sense of it and explore perspectives to regain positive energy. With respect to
         your
         individuality, we understand your experience are unique and important. We help you get closer to them,
