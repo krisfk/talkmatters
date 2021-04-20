989c65861ca299d192138a9ca647fe3756347f66
kaiyu li <krisfk@gmail.com>
2021-04-21 03:25:13 +0800

    .

--- wp-content/themes/talkmatters/page-about.php
+++ wp-content/themes/talkmatters/page-about.php
@@ -28,7 +28,7 @@ get_header();
     </div>
 
 
-    <div class="inner-container mt-4">
+    <div class="inner-container mt-5">
 
         <div class="position-relative">
             <div class="h2-black-line"></div>
