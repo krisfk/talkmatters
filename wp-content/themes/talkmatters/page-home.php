07766d0a30dd71b1caf2ef79c3e4708f71eea5fc
kaiyu li <krisfk@gmail.com>
2021-04-22 16:04:08 +0800

    .

--- wp-content/themes/talkmatters/page-home.php
+++ wp-content/themes/talkmatters/page-home.php
@@ -95,7 +95,7 @@ get_header();
         <div class="row mt-3">
             <div class="col-6"><img class="w-100" src="http://104.131.45.33/wp-content/uploads/2021/04/pens.jpg
 " alt=""></div>
-            <div class="col-6">
+            <div class="col-6  text-justify">
                 No matter how different they look, they are pens for writing. Our emotions function in the same way.
                 They connect with us in different modes and forms, be it happiness, pride, stress, anxiety, anger , they
                 have something important to tell you.
