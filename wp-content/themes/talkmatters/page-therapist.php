921400de306873da533acd49e86273ff2fa6dc38
kaiyu li <krisfk@gmail.com>
2021-04-26 06:22:55 +0800

    .

--- wp-content/themes/talkmatters/page-therapist.php
+++ wp-content/themes/talkmatters/page-therapist.php
@@ -24,6 +24,8 @@ get_header();
     <div class="inner-container mt-const position-relative">
 
         <img src="https://talkmatters.com.hk/wp-content/uploads/2021/04/lilian-photo.jpg" alt="">
+        <?php echo get_field('content_1');?>
+
         <div class="mint-color-rect"></div>
     </div>
 
@@ -32,16 +34,20 @@ get_header();
 
         <div class="position-relative">
             <div class="h2-black-line"></div>
-            <h2 class="h2-on-line">About The Therapist</h2>
-            <h2 class="h2-on-line opacity-0">About The Therapist</h2>
+
+            <h2>
+                <!-- About The Therapist -->
+                <?php echo get_field('content_2');?>
+            </h2>
 
         </div>
 
         <div class="mt-2  text-justify">
-            Lilian believes that each individual is unique and deserves a personalized therapeutic approach. She
+            <!-- Lilian believes that each individual is unique and deserves a personalized therapeutic approach. She
             practices with a selection of interventions based on psychodynamic theories and individual profile. The
             therapeutic process is perceived as a partnership between client and therapist. She pursues for an outcome
-            to empower the client in the reclaiming of wellness and positivity.
+            to empower the client in the reclaiming of wellness and positivity. -->
+            <?php echo get_field('content_3');?>
 
 
         </div>
@@ -55,20 +61,27 @@ get_header();
 
 
                 <div class="col-12">
-                    <h3>Education and Counselling Experience
+                    <h3>
+                        <!-- Education and Counselling Experience -->
+                        <?php echo get_field('content_4');?>
+
                     </h3>
-                    Lilian possesses a Master Degree in Social Sciences (Counselling) from the University of Hong Kong
+                    <!-- Lilian possesses a Master Degree in Social Sciences (Counselling) from the University of Hong Kong
                     and is a member of Hong Kong Professional Counselling Association. She has extensive experiences
                     working with adolescents in a secondary school setting, dealing with issues such as general anxiety,
                     low self esteem, learning and focus. With adults, she has handled cases related to parenting, anger
-                    and couple relationship; as well as anxiety management at work.
+                    and couple relationship; as well as anxiety management at work. -->
+                    <?php echo get_field('content_5');?>
 
-                    <h3 class="mt-4">Other Experiences
+                    <h3 class="mt-4">
+                        <!-- Other Experiences -->
+                        <?php echo get_field('content_6');?>
 
                     </h3>
-                    Lilian also has the capability of a coaching and mentorship. She holds the following experience:
+                    <!-- Lilian also has the capability of a coaching and mentorship. She holds the following experience: -->
+                    <?php echo get_field('content_7');?>
 
-                    <ul class="mt-4">
+                    <!-- <ul class="mt-4">
                         <li>Volunteer Mentor and Counsellor of HKU Counselling Unit (2020 till now)</li>
                         <li>Mentor of CUHK School of Journalism and Communication Taught Postgraduate Mentorship
                             Programme
@@ -78,7 +91,9 @@ get_header();
                             (2016 - 2019)</li>
                         <li>Volunteer Coach at workplace (2013 - 2020)</li>
 
-                    </ul>
+                    </ul> -->
+                    <?php echo get_field('content_8');?>
+
 
 
 
@@ -92,15 +107,20 @@ get_header();
         <div class="mt-5 mb-5">
 
             <div>
-                <h2>Cognitive Behavioral Therapy
+                <h2>
+                    <!-- Cognitive Behavioral Therapy -->
+                    <?php echo get_field('content_9');?>
+
                 </h2>
 
                 <div class="mt-2  text-justify">
-                    A widely used intervention for its proven efficacy and goal-oriented approach. It helps to
+                    <!-- A widely used intervention for its proven efficacy and goal-oriented approach. It helps to
                     investigate
                     how one’s thought patterns work and how it may lead to distress, maladaptive behaviors and mental
                     issues. Ongoing homework is a key component to drive for adaptive thoughts and the subsequent
-                    improvement in mood.
+                    improvement in mood. -->
+                    <?php echo get_field('content_10');?>
+
 
 
                 </div>
@@ -108,45 +128,56 @@ get_header();
 
 
             <div class="mt-const">
-                <h2>Mindfulness & Positive Psychology
+                <h2>
+                    <!-- Mindfulness & Positive Psychology -->
+                    <?php echo get_field('content_11');?>
 
                 </h2>
 
                 <div class="mt-2  text-justify">
-                    Founded on the belief that people want to lead meaningful and fulfilling lives. There is a natural
+                    <!-- Founded on the belief that people want to lead meaningful and fulfilling lives. There is a natural
                     tendency to cultivate the best qualities within ourselves, which everyone possesses in different
                     ways. Positive Psychology and its mindfulness component amplify personal strengths, self-compassion
-                    and social connection; while letting individual to heal base on these values.
+                    and social connection; while letting individual to heal base on these values. -->
+                    <?php echo get_field('content_12');?>
 
 
                 </div>
             </div>
 
             <div class="mt-const">
-                <h2>Emotional-Focused therapy
+                <h2>
+                    <!-- Emotional-Focused therapy -->
+                    <?php echo get_field('content_13');?>
+
 
 
                 </h2>
 
                 <div class="mt-2  text-justify">
-                    Gently explores emotions and invites a genuine conversation with the inner self. With empathy and
+                    <!-- Gently explores emotions and invites a genuine conversation with the inner self. With empathy and
                     compassion, it effectively promotes the release of blockages, bringing in greater flexibility to
-                    heal differently.
+                    heal differently. -->
+                    <?php echo get_field('content_14');?>
 
                 </div>
             </div>
 
             <div class="mt-const">
-                <h2>Solution-focused therapy
+                <h2>
+                    <!-- Solution-focused therapy -->
+                    <?php echo get_field('content_15');?>
 
 
 
                 </h2>
 
                 <div class="mt-2  text-justify">
-                    A goal-directed approach with limited reference made to the context of the emotional issues. Its
+                    <!-- A goal-directed approach with limited reference made to the context of the emotional issues. Its
                     primary focus is on the present and future; and on addressing HOW to resolve clients’ disturbing
-                    matters.
+                    matters. -->
+                    <?php echo get_field('content_16');?>
+
 
 
                 </div>
