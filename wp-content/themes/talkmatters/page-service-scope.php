fdd011c38ab5b67b3cdc4086317b65a6bdd59e0f
kaiyu li <krisfk@gmail.com>
2021-04-26 00:26:41 +0800

    .

--- wp-content/themes/talkmatters/page-service-scope.php
+++ wp-content/themes/talkmatters/page-service-scope.php
@@ -32,7 +32,7 @@ get_header();
         distress and negative emotions that are hard for an individual to manage and maneuver.
     </div>
 
-    <div class="inner-container mt-5 mb-5">
+    <div class="inner-container mt-const">
 
         <div class="scope-points ">
 
