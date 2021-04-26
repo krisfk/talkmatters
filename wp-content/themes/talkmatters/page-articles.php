59ff5cba6d7723cbb34f18d2f76db459ed8aa0f3
kaiyu li <krisfk@gmail.com>
2021-04-27 03:31:12 +0800

    .

--- wp-content/themes/talkmatters/page-articles.php
+++ wp-content/themes/talkmatters/page-articles.php
@@ -42,8 +42,39 @@ get_header();
 
                 <?php while ( $query->have_posts() ) { 
                     $query->the_post(); 
+                    echo 
+                    get_the_post_thumbnail_url() ;
+?>
+                <div class="post-short-div">
+                    <img class="w-100"
+                        src="https://demo.wpexplorer.com/blogger/wp-content/uploads/sites/15/2012/03/hike.jpg" alt="">
 
-echo 1;
+                    <h2 class="mt-3">Reach New Heights</h2>
+                    <ul class="post-meta mt-2">
+                        <li class="meta-date">
+                            Posted on <span class="meta-date-text">November 10, 2012</span>
+                        </li>
+                        <li class="meta-category">
+                            <span class="meta-seperator">/</span>Category: <a
+                                href="https://demo.wpexplorer.com/blogger/category/neat-finds/" title="Neat Finds">Neat
+                                Finds</a>, <a href="https://demo.wpexplorer.com/blogger/category/travel/"
+                                title="Travel">Travel</a>
+                        </li>
+
+                    </ul>
+
+                    <div class="mt-3">Nam mattis tincidunt mattis. Vivamus quam est, condimentum sed iaculis sed, congue
+                        ac arcu.
+                        Morbi nec ipsum velit. Praesent elementum, turpis quis vehicula malesuada, elit mauris sodales
+                        felis, ac porta lorem metus eget ante. Etiam vitae mollis enim, et accumsan enim. Ut eu
+                        tristique sem, a ultricies odio. Integer ac…
+                    </div>
+                    <span class="wpex-readmore mt-4 d-block"><a
+                            href="https://demo.wpexplorer.com/blogger/2012/11/10/reach-new-heights/"
+                            title="continue reading" rel="bookmark">continue reading →</a></span>
+                </div>
+                <?php
+                    
                      }
                      
                      }?>
@@ -67,11 +98,7 @@ echo 1;
                                 Finds</a>, <a href="https://demo.wpexplorer.com/blogger/category/travel/"
                                 title="Travel">Travel</a>
                         </li>
-                        <!-- <li class="meta-comments comment-scroll">
-                            <span class="meta-seperator">/</span>With <a
-                                href="https://demo.wpexplorer.com/blogger/2012/11/10/reach-new-heights/#comments"
-                                class="comments-link">4 Comments</a>
-                        </li> -->
+
                     </ul>
 
                     <div class="mt-3">Nam mattis tincidunt mattis. Vivamus quam est, condimentum sed iaculis sed, congue
