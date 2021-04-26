c256ff09d3fe8784fccc4601afce371635f6fa8f
kaiyu li <krisfk@gmail.com>
2021-04-27 05:40:14 +0800

    .

--- wp-content/themes/talkmatters/archive.php
+++ wp-content/themes/talkmatters/archive.php
@@ -25,7 +25,7 @@ get_header();
 
     <div class="blog-inner-container mt-const2">
 
-        <h3>fdsaf</h3>
+        <h3><?php the_archive_title();?></h3>
         <div class="row g-4">
 
             <div class="col-8">
