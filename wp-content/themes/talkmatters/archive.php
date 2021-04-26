cccbc46cc00d8f4f42f46d40fef91a8b2a48686a
kaiyu li <krisfk@gmail.com>
2021-04-27 05:14:05 +0800

    .

--- wp-content/themes/talkmatters/archive.php
+++ wp-content/themes/talkmatters/archive.php
@@ -28,19 +28,12 @@ get_header();
 
             <div class="col-8">
 
-                <!-- query -->
-                <?php
-    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-    $query = new WP_Query( array(
-        'posts_per_page' => 2,
-        'paged' => $paged
-    ) );
-?>
 
-                <?php if ( $query->have_posts() )
+
+                <?php if ( have_posts() )
                 { ?>
 
-                <?php while ( $query->have_posts() ) { 
+                <?php while ( have_posts() ) { 
                     $query->the_post(); 
                     
 ?>
