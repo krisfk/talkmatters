f9cdd7150291c2929a8af1517623900621b0d117
kaiyu li <krisfk@gmail.com>
2021-04-26 14:49:42 +0800

    .

--- wp-content/themes/talkmatters/page-faqs.php
+++ wp-content/themes/talkmatters/page-faqs.php
@@ -23,7 +23,7 @@ get_header();
 
 
 
-    <div class="inner-container mt-const2 faqs-div">
+    <div class="inner-container mt-const2 faqs-div mb-5">
 
         <h3>Who needs counselling?
         </h3>
