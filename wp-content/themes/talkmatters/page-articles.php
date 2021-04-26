83c5cd5b299e12f7dc1c47d05c8eec9a1c1cd5f8
kaiyu li <krisfk@gmail.com>
2021-04-27 04:09:47 +0800

    .

--- wp-content/themes/talkmatters/page-articles.php
+++ wp-content/themes/talkmatters/page-articles.php
@@ -117,8 +117,8 @@ get_header();
             'end_size'     => 2,
             'mid_size'     => 1,
             'prev_next'    => true,
-            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
-            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+            'prev_text'    => sprintf( '<i></i> %1$s', __( '<', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( '>', 'text-domain' ) ),
             'add_args'     => false,
             'add_fragment' => '',
         ) );
