dcb87111673a4031f2094091ecb2748e0c6f5610
kaiyu li <krisfk@gmail.com>
2021-04-26 21:58:31 +0800

    .

--- wp-content/themes/talkmatters/page-articles.php
+++ wp-content/themes/talkmatters/page-articles.php
@@ -24,8 +24,38 @@ get_header();
 
 
     <div class="inner-container mt-const2">
+        <div class="row">
 
-        coming soonfdsf
+            <div class="col-8">
+                <div class="post-short-div">
+                    <img class="w-100"
+                        src="https://demo.wpexplorer.com/blogger/wp-content/uploads/sites/15/2012/03/hike.jpg" alt="">
+
+                    <div>Reach New Heights</div>
+                    <ul class="post-meta clr">
+                        <li class="meta-date">
+                            Posted on <span class="meta-date-text">November 10, 2012</span>
+                        </li>
+                        <li class="meta-category">
+                            <span class="meta-seperator">/</span>Under <a
+                                href="https://demo.wpexplorer.com/blogger/category/neat-finds/" title="Neat Finds">Neat
+                                Finds</a>, <a href="https://demo.wpexplorer.com/blogger/category/travel/"
+                                title="Travel">Travel</a>
+                        </li>
+                        <li class="meta-comments comment-scroll">
+                            <span class="meta-seperator">/</span>With <a
+                                href="https://demo.wpexplorer.com/blogger/2012/11/10/reach-new-heights/#comments"
+                                class="comments-link">4 Comments</a>
+                        </li>
+                    </ul>
+                    <div></div>
+                </div>
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+
+
     </div>
 </div>
 <?php
