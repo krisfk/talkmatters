12741d83f34ab7e666cd7e381b9c9b1b4deaa7a7
kaiyu li <krisfk@gmail.com>
2021-04-27 03:44:25 +0800

    .

--- wp-content/themes/talkmatters/page-articles.php
+++ wp-content/themes/talkmatters/page-articles.php
@@ -62,7 +62,7 @@ get_header();
                             $post_id= get_the_ID();
                             $category_detail=get_the_category($post_id);//$post->ID
                                     foreach($category_detail as $cd){
-                                    echo '<a href="/category/'.$cd->slug.'">'.$cd->cat_name.'</a>'.',';
+                                    echo '<a href="/category/'.$cd->slug.'">'.$cd->cat_name.'</a>'.' , ';
                                     // print_r ($cd);
                                     }
 
