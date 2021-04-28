698774db2a53b58f0333749ddc3d5fa646eb3a93
kaiyu li <krisfk@gmail.com>
2021-04-28 17:51:47 +0800

    .

--- wp-content/themes/talkmatters/page-articles.php
+++ wp-content/themes/talkmatters/page-articles.php
@@ -26,7 +26,7 @@ get_header();
     <div class="blog-inner-container mt-const">
         <div class="row g-4">
 
-            <div class="col-8">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-8 ">
 
                 <!-- query -->
                 <?php
@@ -173,7 +173,7 @@ get_header();
 
 
             </div>
-            <div class="col-4 blog-right-col">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-4 blog-right-col">
 
                 <?php get_template_part( 'nav','blog-side-col' );           // Navigation bar (nav.php) ?>
 
