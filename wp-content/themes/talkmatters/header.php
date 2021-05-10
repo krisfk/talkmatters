b106e3b9e0e39916aebd12fa47742e801422372b
kaiyu li <krisfk@gmail.com>
2021-05-10 23:17:24 +0800

    .

--- wp-content/themes/talkmatters/header.php
+++ wp-content/themes/talkmatters/header.php
@@ -64,7 +64,7 @@ if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
                         <ul class="header-ul  pb-2">
                             <li>
                                 <span class="site-name">
-                                    Talkmatters <br />Psychological Counselling </span>
+                                    TalkMatters <br />Psychological Counselling </span>
                             </li>
                             <!-- <li class="ps-4">
 
