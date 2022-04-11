112fe6029a30d40cbaeb0f34366da82e6e4d5541
kaiyu li <krisfk@gmail.com>
2022-04-12 03:35:43 +0800

    .

--- wp-content/themes/talkmatters/page-corporate-service.php
+++ wp-content/themes/talkmatters/page-corporate-service.php
@@ -19,7 +19,7 @@ get_header();
 ?>
 <div class="container">
 
-    <div>
+    <div class=" inner-container">
 
         We believe that “people” is key to the success of a group. A respect of individuality and diversity in a system
         is paramount. TalkMatters partners with corporates in its people experience strategy. As an enabler of
