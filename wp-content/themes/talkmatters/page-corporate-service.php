23845537d9fba66781cbc7283ac0ace2ac81f177
kaiyu li <krisfk@gmail.com>
2022-05-06 14:05:08 +0800

    .

--- wp-content/themes/talkmatters/page-corporate-service.php
+++ wp-content/themes/talkmatters/page-corporate-service.php
@@ -22,44 +22,51 @@ get_header();
     <div class=" inner-container  mt-const">
 
         <div class="mt-2 text-justify">
-            We believe that “people” is key to the success of a group. A respect of individuality and diversity in a
+            <?php echo get_field('content_1');?>
+            <!-- We believe that “people” is key to the success of a group. A respect of individuality and diversity in a
             system
             is paramount. TalkMatters partners with corporates in its people experience strategy. As an enabler of
             individual wellness and commitment, we lead the process in an innovative way. It involves making sense of
             your
             unique culture & dynamics, identifying mutual purposes and curating sustainable perspectives with your
             employees. The outcome is empowerment that contributes to a collective movement, from Effectiveness to
-            Greatness.
+            Greatness. -->
 
         </div>
     </div>
 
     <div class=" mt-const lilian-therapy-div inner-container mint-color home-slogan text-justify mint-bg p-4">
 
-        <h3 class="">Service Scope </h3>
+        <h3 class=""><?php echo get_field('content_2');?>
+            <!-- Service Scope  -->
+        </h3>
         <h3 class="mt-4">
-            Talk & Workshop
+            <?php echo get_field('content_3');?>
+            <!-- Talk & Workshop -->
 
         </h3>
-        <ul class="">
+        <?php echo get_field('content_4');?>
+        <!-- <ul class="">
             <li> Employee Wellness - Mindfulness, Stress Reduction, Relationship</li>
             <li> Leadership - Communications & Etiquette</li>
             <li> Transformation & Change Management</li>
 
-        </ul>
+        </ul> -->
 
         <h3 class="mt-4">
-            One-on-one Services
+            <?php echo get_field('content_5');?>
+            <!-- One-on-one Services -->
 
 
         </h3>
-        <ul>
+        <?php echo get_field('content_6');?>
+        <!-- <ul>
             <li> Executive Coaching
             </li>
             <li> Psychological Counselling
             </li>
 
-        </ul>
+        </ul> -->
 
 
     </div>
@@ -72,7 +79,8 @@ get_header();
 
 
 
-        <a href="/events/" class="btn mt-3 contact-us-btn"> Testimonials <?php //echo get_field('content_9');?>
+        <a href="/events/" class="btn mt-3 contact-us-btn"> <?php echo get_field('content_7');?>
+            <!-- Testimonials -->
         </a>
     </div>
 </div>
